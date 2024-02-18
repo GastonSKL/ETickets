4754e1f1c68fb06893e8deac3a1b247a1e7bb218
GastonSKL <gastonlskamlec@gmail.com>
2024-02-18 17:57:57 -0300

    Agregado de esquelos de controladores

---
+++ ETickets/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.ToListAsync();
+
+            return View();
+        }
+    }
+}
---
+++ ETickets/Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Actors.ToListAsync();
+
+            return View();
+        }
+    }
+}
